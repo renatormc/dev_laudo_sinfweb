f1ee13c88ae0c31b08e4054ed96cc005e4e57960
renatormc <renatomartinsrmc@gmail.com>
2022-03-28 14:18:55 -0300

    Normal

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
 import shutil
-from helpers import choose_model, get_test_context
 import config
-from report_writer import Renderer
+
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest="command", required=True, help='Command to be used')
@@ -13,15 +12,18 @@ p_start = subparsers.add_parser("start")
 
 
 args = parser.parse_args()
-if args.model == "choose":
-    args.model = choose_model()
-config.model = args.model
 
-if args.command == "render":
-    r = Renderer(config.model, config.app_dir / "models")
-    context = get_test_context(config.model)
-    path = r.render("", context, only_laudo=True)
-    shutil.move(path, config.app_dir / "compilado.docx")
-    print("Renderizado arquivo compilado.docx")
-elif args.command == "start":
+if args.command == "start":
     shutil.copytree(config.app_dir / "models_example", config.app_dir / "models")
+else:
+    from helpers import choose_model, get_test_context
+    from report_writer import Renderer
+    if args.model == "choose":
+        args.model = choose_model()
+        config.model = args.model
+    if args.command == "render":
+        r = Renderer(config.model, config.app_dir / "models")
+        context = get_test_context(config.model)
+        path = r.render("", context, only_laudo=True)
+        shutil.move(path, config.app_dir / "compilado.docx")
+        print("Renderizado arquivo compilado.docx")

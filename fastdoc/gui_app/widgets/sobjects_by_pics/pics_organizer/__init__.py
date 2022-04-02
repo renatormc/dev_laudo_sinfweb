51a3268490a149d972038f669eafcb5457e79828
renatormc <renatomartinsrmc@gmail.com>
2022-04-02 20:58:32 -0300

    Normal

--- fastdoc/gui_app/widgets/sobjects_by_pics/pics_organizer/__init__.py
+++ fastdoc/gui_app/widgets/sobjects_by_pics/pics_organizer/__init__.py
@@ -1,8 +1,11 @@
+from copy import copy
+from pathlib import Path
 from .pics_organizer_ui import Ui_PicsOrganizer
-from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel, QListWidgetItem
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel, QListWidgetItem, QMenu, QAction, QListWidget
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import QSize
 from .organizer_obj import OrganizerObj
+
 
 class PicsOrganizer(QDialog):
     def __init__(self, objects):
@@ -12,28 +15,97 @@ class PicsOrganizer(QDialog):
         self.ui.setupUi(self)
         self.ui.led_alias_pericias.setText(objects['alias'])
         self.connections()
-        self.objects_widgets: list[OrganizerObj] = []
+        self.objects_widgets: dict[int, OrganizerObj] = {}
+        self.change_icon_size(self.ui.sld_icon_size.value())
+        self.reload()
 
     def connections(self):
-        self.ui.btn_reconfigure.clicked.connect(self.reconfig)
+        self.ui.btn_add.clicked.connect(self.add_obbjects)
+        self.ui.sld_icon_size.valueChanged.connect(self.change_icon_size)
+        self.ui.lsw_not_associated.customContextMenuRequested.connect(self.provide_context_menu_default)
+    
 
-    def reconfig(self):
-        for obj in self.objects_widgets:
-            obj.deleteLater()
-        self.objects_widgets = []
-        for i in range(self.ui.spb_n_objetos.value()):
-            obj = OrganizerObj()
-            obj.set_object_types(['Celular', 'Computador', 'HDD'])
-            obj.set_name(f"Objeto {i+1}")
-            self.ui.lay_objects.addWidget(obj)
-            self.objects_widgets.append(obj)
-        # self.ui.lsw_others.setIconSize(QSize(150,150))
+    def reload(self):
+        self.ui.lsw_not_associated.clear()
         for obj in self.objects['objects']:
             for pic in obj['pics']:
                 item = QListWidgetItem()
                 icon = QIcon()
-                icon.addPixmap(QPixmap(pic),QIcon.Normal, QIcon.Off)
+                icon.addPixmap(QPixmap(pic), QIcon.Normal, QIcon.Off)
                 item.setIcon(icon)
+                path = Path(pic)
+                item.setText(path.name)
                 self.ui.lsw_not_associated.addItem(item)
 
+    def add_object(self) -> OrganizerObj:
+        n = len(self.objects_widgets.keys())
+        index = n + 1
+        obj = OrganizerObj(index)
+        obj.set_object_types(['Celular', 'Computador', 'HDD'])
+        self.ui.lay_objects.addWidget(obj)
+        self.objects_widgets[index] = obj
+        obj.close_clicked.connect(self.remove_object)
+        obj.context_menu_requested.connect(self.provide_context_menu)
+        obj.set_icon_size(QSize(10000, self.ui.sld_icon_size.value()))
+        return obj
 
+    def add_obbjects(self):
+        for i in range(self.ui.spb_n_add.value()):
+            self.add_object()
+
+    def change_icon_size(self, value):
+        self.ui.lsw_not_associated.setIconSize(QSize(10000, value))
+        for item in self.objects_widgets.values():
+            item.set_icon_size(QSize(10000, value))
+
+    def remove_object(self, index: int):
+        objw = self.objects_widgets[index].ui.lsw_object
+        for i in range(objw.count()):
+            self.move_item(objw, self.ui.lsw_not_associated, objw.item(i), copy=True)
+        self.objects_widgets[index].deleteLater()
+        del self.objects_widgets[index]
+
+    def move_item(self, lsw_from: QListWidget, lsw_to: QListWidget, item: QListWidgetItem, copy=False):
+        item_clone = item.clone()
+        lsw_to.addItem(item_clone)
+        if not copy:
+            lsw_from.takeItem(lsw_from.row(item))
+
+    def provide_context_menu_default(self, pos):
+        self.provide_context_menu(pos, self.ui.lsw_not_associated)
+
+    def provide_context_menu(self, pos, lsw: QListWidget):
+        menu = QMenu()
+
+        act_move_new = QAction('Mover para novo ojeto')
+        menu.addAction(act_move_new)
+
+        if lsw != self.ui.lsw_not_associated:
+            menu.addAction("Remover do objeto")
+
+        objects_actions = {}
+        for index in self.objects_widgets.keys():
+            text = f"Adicionar ao objeto {index}"
+            objects_actions[text] = index
+            menu.addAction(text)
+
+        act = menu.exec_(lsw.mapToGlobal(pos))
+        if act:
+            if act == act_move_new:
+                obj = self.add_object()
+                objw = obj.ui.lsw_object
+                for item in lsw.selectedItems():
+                    self.move_item(lsw, objw, item)
+            elif act.text() == "Remover do objeto":
+                for item in lsw.selectedItems():
+                    self.move_item(lsw, self.ui.lsw_not_associated, item)
+            else:
+                try:
+                    index = objects_actions[act.text()]
+                    obj = self.objects_widgets[index]
+                    objw = obj.ui.lsw_object
+                    for item in lsw.selectedItems():
+                        self.move_item(lsw, objw, item)
+                except KeyError:
+                    pass
+        
--- fastdoc/gui_app/widgets/sobjects_by_pics/pics_organizer/organizer_obj/__init__.py
+++ fastdoc/gui_app/widgets/sobjects_by_pics/pics_organizer/organizer_obj/__init__.py
@@ -1,22 +1,46 @@
 from .organizer_obj_ui import Ui_OrganizerObj
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QListWidget
+from PyQt5.QtCore import pyqtSignal, QSize, QPoint
+
 
 class OrganizerObj(QWidget):
-    def __init__(self):
+    close_clicked = pyqtSignal(int)
+    context_menu_requested = pyqtSignal(QPoint, QListWidget)
+
+    def __init__(self, index: int):
         super(self.__class__, self).__init__()
+        self._index = 0
         self.ui = Ui_OrganizerObj()
         self.ui.setupUi(self)
+        self.index = index
         self.connections()
 
+    @property
+    def index(self) -> int:
+        return self._index
+
+    @index.setter
+    def index(self, value: int) -> None:
+        self._index = value
+        self.ui.lbl_name.setText(f"Objeto {value}")
+
     def connections(self):
-        pass
+        self.ui.btn_close.clicked.connect(self.close_button_pressed)
+        self.ui.lsw_object.customContextMenuRequested.connect(self.provide_context_menu)
+
+    def set_icon_size(self, size: QSize):
+        self.ui.lsw_object.setIconSize(size)
+
 
     def set_object_types(self, types: list[str]):
         self.ui.cbx_type.clear()
         self.ui.cbx_type.addItems(types)
 
-    def set_name(self, value):
-        self.ui.lbl_name.setText(value)
-
     def get_type(self) -> str:
         return self.ui.cbx_type.currentText()
+
+    def close_button_pressed(self):
+        self.close_clicked.emit(self._index)
+
+    def provide_context_menu(self, pos):
+        self.context_menu_requested.emit(pos, self.ui.lsw_object)

3fb1f792bf4febf4731e05d5e32fd9b365be6fb4
renatormc <renatomartinsrmc@gmail.com>
2022-04-17 22:42:36 -0300

    Normal

--- fastdoc/models_example/sinf_generico/initial_feeder.py
+++ fastdoc/models_example/sinf_generico/initial_feeder.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Optional, Union
 from rlibs.report_writer.types import InitialData
+from fastdoc.data_extractors.odin_pdf_parser import OdinPdfParser
 
 
 def get_initial_data(workdir: Union[Path, str]) -> Optional[InitialData]:
@@ -9,4 +10,18 @@ def get_initial_data(workdir: Union[Path, str]) -> Optional[InitialData]:
     d.form_data = {
         "objects": str(pics_folder.absolute())
     }
+    path = Path(workdir) / "Requisicao.pdf"
+    if path.exists():
+        parser = OdinPdfParser(path)
+        data = parser.extract_all()
+        print(data)
+        p = data['pericia']
+        d.form_data['pericia'] = f"{p['seq']}/{p['rg']}/{p['ano']}"
+        d.form_data['requisitante'] = data['quesito']['unidade_origem']
+        d.form_data['procedimento'] = f"RAI {data['rai']}"
+        d.form_data['ocorrencia_odin'] = data['ocorrencia']
+        d.form_data['data_odin'] = data['data_ocorrencia']
+        d.form_data['numero_quesito'] = data['quesito']['numero']
+        d.form_data['autoridade'] = data['autoridade']
+
     return d

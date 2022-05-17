d9a56f59ec335855e6549edf7446d6832038dab2
renatormc <renatomartinsrmc@gmail.com>
2022-05-16 23:25:43 -0300

    Normal

--- fastdoc/updater/updater.py
+++ fastdoc/updater/updater.py
@@ -5,15 +5,20 @@ import sys
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 python_exe = Path(sys.executable)
-git_exe =  (app_dir / "../../extras/GitPortable/App/Git/bin/git.exe").absolute()
+python_folder = python_exe.parent
+git_bin =  (app_dir / "../../extras/GitPortable/App/Git/bin").absolute()
+current_path = os.getenv("PATH")
 
-subprocess.check_output([str(git_exe), 'reset', '--hard'])
-subprocess.check_output([str(git_exe), 'checkout', 'master'])
-subprocess.check_output([str(git_exe), 'pull','origin', 'master'])
+new_path = f"{python_folder};{python_folder / 'Scripts'};{git_bin};{current_path}"
+os.environ['PATH'] = new_path
 
-subprocess.check_output([str(python_exe), "-m", "pip", "install", "-r", "requirements.txt"])
-subprocess.check_output([str(python_exe), "manage.py", "db_upgrade"])
-subprocess.check_output([str(python_exe), "main.py", "analise-models-folder"])
+subprocess.check_output(['git', 'reset', '--hard'])
+subprocess.check_output(['git', 'checkout', 'master'])
+subprocess.check_output(['git', 'pull','origin', 'master'])
+
+subprocess.check_output(['python', "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.check_output(['python', "manage.py", "db_upgrade"])
+subprocess.check_output(['python', "main.py", "analise-models-folder"])
 
 print("Aplicativo atualizado com sucesso!")
 
